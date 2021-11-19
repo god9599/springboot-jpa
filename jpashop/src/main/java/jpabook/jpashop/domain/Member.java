cc7b5abea56f766975d57e494e869aa1bb573f5f
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-11-19 10:05:44 +0900

    Chore :: entity class 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.order.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class Member {
+    @Column(name = "member_id")
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // order 테이블에 있는 member 필드에 의해 매핑
+    private List<Order> orders = new ArrayList<>();
+
+}
